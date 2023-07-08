d9a94a9551f41f858dde7da4e143a0c74f01405b
kimojune <rlaghwns4848@naver.com>
2023-07-08 18:38:54 +0900

    Add Image file

---
+++ Engine_Source/yaImage.h
@@ -0,0 +1,29 @@
+#include "yaResource.h"
+
+namespace ya
+{
+	class Image : public Resource
+	{
+	public:
+		static std::shared_ptr<Image> Create(const std::wstring& name, UINT widht, UINT height, COLORREF rgb = RGB(255, 255, 255));
+
+		Image();
+		~Image();
+
+		virtual HRESULT Load(const std::wstring& path) override;
+
+		HDC GetHdc() { return mHdc; }
+		HBITMAP GetBitmap() { return mBitmap; }
+		UINT GetWidth() { return mWidth; }
+		UINT GetHeight() { return mHeight; }
+
+		COLORREF GetPixel(int x, int y);
+		void SetPixel(int x, int y, COLORREF color);
+
+	private:
+		HBITMAP mBitmap;
+		HDC mHdc;
+		UINT mWidth;
+		UINT mHeight;
+	};
+}
